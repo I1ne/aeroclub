93a1c86c4154aed7902ef7e355c3b24cd0121851
I1ne <142399353+I1ne@users.noreply.github.com>
2025-01-08 18:42:06 +0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Для_курсача
+{
+    public partial class App : Application //Класс App является точкой входа для приложения. Он определяет общие настройки и поведение приложения
+    {
+        public App() //Вызывается при запуске приложения 
+        {
+            InitializeComponent();
+
+            // Указываем, что WelcomePage будет главной страницей приложения
+            MainPage = new NavigationPage(new WelcomePage());
+        }
+
+        protected override void OnStart() //Вызывается при запуске приложения
+        {
+        }
+
+        protected override void OnSleep() //Вызывается, когда приложение сворчавивается 
+        {
+        }
+
+        protected override void OnResume() //Вызывается, когда приложение возвращается из состояния свёрнутости
+        {
+        }
+    }
+}
